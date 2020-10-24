0ce3fc972a23d4be875c29286959186198d27d1f
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2020-10-24 03:18:08 +0200

    Unified tests packages naming schema

---
+++ remote/src/test/groovy/video/remote/tests/BaseSpec.groovy
@@ -0,0 +1,30 @@
+package video.remote.tests
+
+import com.automation.remarks.remote.StartGrid
+import com.automation.remarks.video.recorder.monte.MonteRecorder
+import spock.lang.Specification
+
+import static java.lang.Thread.sleep
+
+/**
+ * Created by sergey on 5/14/16.
+ */
+abstract class BaseSpec extends Specification {
+
+    final String NODE_SERVLET_URL = "http://localhost:5555/extra/Video"
+
+    def setupSpec() {
+        runGrid()
+    }
+
+    def runGrid() {
+        String[] args = []
+        StartGrid.main(args)
+        sleep(1000)
+    }
+
+    def getVideoFiles(){
+        new File(MonteRecorder.conf().folder()).listFiles()
+    }
+
+}
