5b7fe9e8130b3f99ed1b740c92b66913fddfa10a
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2020-10-24 04:03:46 +0200

    Reformat DateUtils class

--- core/src/main/java/com/automation/remarks/video/DateUtils.java
+++ core/src/main/java/com/automation/remarks/video/DateUtils.java
@@ -2,15 +2,17 @@ package com.automation.remarks.video;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by sepi on 15.07.16.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateUtils {
 
-  public static String formatDate(Date date, String format) {
-    SimpleDateFormat dateFormat = new SimpleDateFormat(
-        format);
-    return dateFormat.format(date);
-  }
+    public static String formatDate(Date date, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
 }
